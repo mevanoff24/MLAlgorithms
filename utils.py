0b026dfbe98645a2557215b62cd208b1bdd56aad
Matt Evanoff <mevanoff24@gmail.com>
2019-04-07 19:32:34 -0700

    added gini and entropy

--- utils.py
+++ utils.py
@@ -44,3 +44,33 @@ def cross_entropy_fastest(y_true, y_pred):
 def euclidean_distance(a, b, axis=1):
     return np.linalg.norm(a - b, axis=axis)
 
+
+def entropy(y):
+    # assume y is binary - 0 or 1
+    N = len(y)
+    s1 = (y == 1).sum()
+    if 0 == s1 or N == s1:
+        return 0
+    p1 = float(s1) / N
+    p0 = 1 - p1
+    return -p0 * np.log2(p0) - p1 * np.log2(p1)
+
+def gini_index(groups, classes):
+    # count all samples at split point
+    n_instances = float(sum([len(group) for group in groups]))
+    # sum weighted Gini index for each group
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        # avoid divide by zero
+        if size == 0:
+            continue
+        score = 0.0
+        # score the group based on the score for each class
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        # weight the group score by its relative size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
